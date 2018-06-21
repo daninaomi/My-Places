13f363ba572a5fa6662f08e690576d4c5e8756c4
Nakatsu <danielle.n.nakatsu@accenture.com>
2018-06-21 17:39:13 -0300

    icons config

--- App.js
+++ App.js
@@ -1,8 +1,3 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
 
 import React, { Component } from 'react';
 import {
@@ -12,6 +7,8 @@ import {
   FlatList,
   Image
 } from 'react-native';
+
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 const url = 'https://maps.googleapis.com/maps/api/place/photo?'
 const urlFoto = 'https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=CnRtAAAATLZNl354RwP_9UKbQ_5Psy40texXePv4oAlgP4qNEkdIrkyse7rPXYGd9D_Uj1rVsQdWT4oRz4QrYAJNpFX7rzqqMlZw2h2E2y5IKMUZ7ouD_SlcHxYq1yL4KbKUv3qtWgTK0A6QbGh87GB3sscrHRIQiG2RrmU_jF4tENr9wGS_YxoUSSDrYjWmrNfeEHSGSc3FyhNLlBU&key=AIzaSyDkBXbmvqlnaINSdDhVT89Y5Zc6sk5ny3k'
@@ -85,6 +82,7 @@ export default class App extends Component {
           data={this.state.locais}
           renderItem={({ item }) =>
             <View>
+              <Icon name="map-marker" size={30} color="#900" />
               <Text>
                 {item.name}
               </Text>
